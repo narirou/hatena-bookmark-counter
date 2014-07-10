de0ec857077879238169708751e284aa8f60b4a4
narirou <narirou.github@gmail.com>
2014-07-11 06:39:20 +0900

    add meta.js

---
+++ hatena-bookmark-counter.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           Hatena Bookmark Counter
+// @version        0.3.1
+// @namespace      https://github.com/narirou/
+// @author         narirou
+// @description    Add hatena bookmark count to the search results.
+// @include        http://www.google.tld/*
+// @include        https://www.google.tld/*
+// @include        http://search.yahoo.co.jp/*
+// @include        https://search.yahoo.co.jp/*
+// @updateURL      https://raw.githubusercontent.com/narirou/hatena-bookmark-counter/master/hatena-bookmark-counter.meta.js
+// @grant          GM_xmlhttpRequest
+// @grant          GM_addStyle
+// @run-at         document-end
+// @license        MIT Lisense
+// ==/UserScript==
